1a4f525b23ddb19340d97f6818e3ccd4b487d4f1
mahindipto <mahindipto18@gmail.com>
2024-06-09 19:13:51 +0600

    added scroll to top

--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../Components/Shared/NavBar";
 import Footer from "../Components/Shared/Footer";
+import ScrollToTop from "../Components/ScrollToTop";
 
 const Root = () => {
     return (
         <div>
             <NavBar></NavBar>
+            <ScrollToTop />
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider } from 'react-router-dom'
 import router from './Routes/Routes'
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
