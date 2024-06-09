7905f3dce29f16ea196fd34c063eb0929a6f47f5
mahindipto <mahindipto18@gmail.com>
2024-06-09 16:18:46 +0600

    final check

--- src/Components/Shared/NavBar.jsx
+++ src/Components/Shared/NavBar.jsx
@@ -1,3 +1,4 @@
+
 import { Link, NavLink } from "react-router-dom";
 const NavBar = () => {
 
@@ -9,31 +10,31 @@ const NavBar = () => {
                         return {
                             color: isActive ? "red" : "black",
                             backgroundColor: isActive ? "inherit" : "inherit",
-                            textDecoration: isActive? 'underline': "none" 
+                            textDecoration: isActive ? 'underline' : "none"
                         };
                     }} to="/"
                     >Home</NavLink></li>
                     <li><NavLink className="py-2 font-bold" style={({ isActive }) => {
-                          return {
+                        return {
                             color: isActive ? "red" : "black",
                             backgroundColor: isActive ? "inherit" : "inherit",
-                            textDecoration: isActive? 'underline': "none" 
+                            textDecoration: isActive ? 'underline' : "none"
                         };
                     }} to="/products"
                     >Products</NavLink></li>
                     <li><NavLink className="py-2 font-bold" style={({ isActive }) => {
-                          return {
+                        return {
                             color: isActive ? "red" : "black",
                             backgroundColor: isActive ? "inherit" : "inherit",
-                            textDecoration: isActive? 'underline': "none" 
+                            textDecoration: isActive ? 'underline' : "none"
                         };
                     }} to="/Services"
                     >Services</NavLink></li>
                     <li><NavLink className="py-2 font-bold" style={({ isActive }) => {
-                         return {
+                        return {
                             color: isActive ? "red" : "black",
                             backgroundColor: isActive ? "inherit" : "inherit",
-                            textDecoration: isActive? 'underline': "none" 
+                            textDecoration: isActive ? 'underline' : "none"
                         };
                     }} to="/contact"
                     >Contact us</NavLink></li>
@@ -42,10 +43,10 @@ const NavBar = () => {
                             <summary className="py-2 font-bold">About US</summary>
                             <ul className="p-2">
                                 <li><NavLink className="py-2 font-bold" style={({ isActive }) => {
-                                     return {
+                                    return {
                                         color: isActive ? "red" : "black",
                                         backgroundColor: isActive ? "inherit" : "inherit",
-                                        textDecoration: isActive? 'underline': "none" 
+                                        textDecoration: isActive ? 'underline' : "none"
                                     };
                                 }} to="/aboutUS"
                                 >About Golden Boiler</NavLink></li>
@@ -53,15 +54,15 @@ const NavBar = () => {
                                     return {
                                         color: isActive ? "red" : "black",
                                         backgroundColor: isActive ? "inherit" : "inherit",
-                                        textDecoration: isActive? 'underline': "none" 
+                                        textDecoration: isActive ? 'underline' : "none"
                                     };
                                 }} to="/massage"
                                 >Massage From MD</NavLink></li>
                                 <li><NavLink className="py-2 font-bold" style={({ isActive }) => {
-                                     return {
+                                    return {
                                         color: isActive ? "red" : "black",
                                         backgroundColor: isActive ? "inherit" : "inherit",
-                                        textDecoration: isActive? 'underline': "none" 
+                                        textDecoration: isActive ? 'underline' : "none"
                                     };
                                 }} to="/gallery"
                                 >Gallery</NavLink></li>
@@ -73,22 +74,13 @@ const NavBar = () => {
             </ul>
         </>
     return (
-        <div className="navbar bg-inherit bg-opacity-30  mx-auto  fixed z-10">
+        <div className={"navbar bg-white  mx-auto  fixed z-10"}>
             <div className="navbar-start ml-5">
                 <div className="dropdown mr-3">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        {/* <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li> */}
                         {
                             navLinks
                         }
